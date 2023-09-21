e5027632c30a30287509415cb573c408d312628f
transunlight <thetruthshaper@gmail.com>
2023-09-21 14:16:05 +0530

    Add class Inventory

--- base/inventory.py
+++ base/inventory.py
@@ -1,5 +1,6 @@
 """
 Module defining classes:
+- Inventory
 - Item
 
 """
@@ -7,8 +8,9 @@ Module defining classes:
 from __future__ import annotations
 
 from fractions import Fraction
+from math import ceil
 
-from attrs import frozen
+from attrs import define, field, frozen
 
 from .utils import TechLevel
 
@@ -22,3 +24,27 @@ class Item:
     encumbrance: Fraction
     description: str
     tech_level: TechLevel
+
+
+@define
+class Inventory:
+    """Class for inventory"""
+
+    readied: dict[Item, int] = field(factory=dict, init=False)
+    stowed: dict[Item, int] = field(factory=dict, init=False)
+    e_credits: int = field(default=0, init=False)
+
+    @property
+    def readied_encumbrance(self) -> int:
+        """Encumbrance of all readied items"""
+        return sum(ceil(item.encumbrance * cnt) for item, cnt in self.readied.items())
+
+    @property
+    def stowed_encumbrance(self) -> int:
+        """Encumbrance of all stowed items"""
+        return sum(ceil(item.encumbrance * cnt) for item, cnt in self.stowed.items())
+
+    @property
+    def credits(self) -> int:
+        """Total credits"""
+        return self.e_credits
