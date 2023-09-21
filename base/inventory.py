121807016f68a6469ad9a269cfcc72e13f79288a
transunlight <thetruthshaper@gmail.com>
2023-09-21 14:20:16 +0530

    Add class Item

--- base/inventory.py
+++ base/inventory.py
@@ -1,6 +1,24 @@
 """
 Module defining classes:
+- Item
 
 """
 
 from __future__ import annotations
+
+from fractions import Fraction
+
+from attrs import frozen
+
+from .utils import TechLevel
+
+
+@frozen
+class Item:
+    """Class for item"""
+
+    cost: int
+    name: str
+    encumbrance: Fraction
+    description: str
+    tech_level: TechLevel
